5d3d6069b1dbe8bd4cf8905908766bf5f926d1b6
EduardoNavarro <eduardo.navarro1984@gmail.com>
2013-10-31 14:35:01 -0300

    Mobile Backend Starter Client
    Firts test client working properly

--- src/com/google/cloud/backend/android/Consts.java
+++ src/com/google/cloud/backend/android/Consts.java
@@ -23,17 +23,17 @@ public interface Consts {
   /**
    * Set Project ID of your Google APIs Console Project.
    */
-  public static final String PROJECT_ID = "*** ENTER YOUR PROJECT ID ***";
+  public static final String PROJECT_ID = "cloud-android-test";
 
   /**
    * Set Project Number of your Google APIs Console Project.
    */
-  public static final String PROJECT_NUMBER = "*** ENTER YOUR PROJECT NUMBER ***";
+  public static final String PROJECT_NUMBER = "890170971276";
 
   /**
    * Set your Web Client ID for authentication at backend.
    */
-  public static final String WEB_CLIENT_ID = "*** ENTER YOUR WEB CLIENT ID ***";
+  public static final String WEB_CLIENT_ID = "AIzaSyAuqSmZnWBJb49_PYacDJO65UMIbndrrio";
 
   /**
    * Set default user authentication enabled or disabled.
--- src/com/google/cloud/backend/android/sample/guestbook/GuestbookActivity.java
+++ src/com/google/cloud/backend/android/sample/guestbook/GuestbookActivity.java
@@ -13,10 +13,25 @@
  */
 package com.google.cloud.backend.android.sample.guestbook;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,131 +42,232 @@ import com.google.cloud.backend.android.CloudQuery.Order;
 import com.google.cloud.backend.android.CloudQuery.Scope;
 import com.google.cloud.backend.android.R;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
-
 /**
  * Sample Guestbook app with Mobile Backend Starter.
  *
  */
 public class GuestbookActivity extends CloudBackendActivity {
 
-  // data formatter for formatting createdAt property
-  private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss ", Locale.US);
+	// data formatter for formatting createdAt property
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss ", Locale.US);
 
-  private static final String BROADCAST_PROP_DURATION = "duration";
+	private static final String BROADCAST_PROP_DURATION = "duration";
 
-  private static final String BROADCAST_PROP_MESSAGE = "message";
+	private static final String BROADCAST_PROP_MESSAGE = "message";
 
-  // UI components
-  private TextView tvPosts;
-  private EditText etMessage;
-  private Button btSend;
+	private Context context;
+	// UI components
+	private TextView tvPosts;
+	private EditText etMessage;
+	private Button btSend;
+	private ProgressBar pgbar;
 
-  // a list of posts on the UI
-  List<CloudEntity> posts = new LinkedList<CloudEntity>();
+	private boolean isAppinForeground = false;
+	private boolean test = false;
+	// a list of posts on the UI
+	List<CloudEntity> posts = new LinkedList<CloudEntity>();
 
-  // initialize UI
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    tvPosts = (TextView) findViewById(R.id.tvPosts);
-    etMessage = (EditText) findViewById(R.id.etMessage);
-    btSend = (Button) findViewById(R.id.btSend);
-  }
+	// initialize UI
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_main);
 
-  @Override
-  protected void onPostCreate() {
-    super.onPostCreate();
-    listAllPosts();
-  }
+			context = this;
+			tvPosts = (TextView) findViewById(R.id.tvPosts);
+			etMessage = (EditText) findViewById(R.id.etMessage);
+			btSend = (Button) findViewById(R.id.btSend);
+			pgbar = (ProgressBar) findViewById(R.id.progressLoading);
+	}
 
-  // execute query "SELECT * FROM Guestbook ORDER BY _createdAt DESC LIMIT 50"
-  // this query will be re-executed when matching entity is updated
-  private void listAllPosts() {
+	@Override
+	protected void onPostCreate() {
+		super.onPostCreate();
+		listAllPosts();
+	}
 
-    // create a response handler that will receive the query result or an error
-    CloudCallbackHandler<List<CloudEntity>> handler = new CloudCallbackHandler<List<CloudEntity>>() {
-      @Override
-      public void onComplete(List<CloudEntity> results) {
-        posts = results;
-        updateGuestbookUI();
-      }
+	// execute query "SELECT * FROM Guestbook ORDER BY _createdAt DESC LIMIT 50"
+	// this query will be re-executed when matching entity is updated
+	private void listAllPosts() {
 
-      @Override
-      public void onError(IOException exception) {
-        handleEndpointException(exception);
-      }
-    };
+		// create a response handler that will receive the query result or an error
+		CloudCallbackHandler<List<CloudEntity>> handler = new CloudCallbackHandler<List<CloudEntity>>() {
+			@Override
+			public void onComplete(List<CloudEntity> results) {
+				posts = results;
+				updateGuestbookUI();
 
-    // execute the query with the handler
-    getCloudBackend().listByKind("Guestbook", CloudEntity.PROP_CREATED_AT, Order.DESC, 50,
-        Scope.FUTURE_AND_PAST, handler);
-  }
+				//if (!isApplicationBroughtToBackground()) {
+				if (isAppinForeground) {
+					PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+					PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "");
+					wl.acquire();
 
-  private void handleEndpointException(IOException e) {
-    Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
-    btSend.setEnabled(true);
-  }
+					Intent intent = new Intent(context, GuestbookActivity.class);
+					PendingIntent pIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-  // convert posts into string and update UI
-  private void updateGuestbookUI() {
-    final StringBuilder sb = new StringBuilder();
-    for (CloudEntity post : posts) {
-      sb.append(sdf.format(post.getCreatedAt()) + getCreatorName(post) + ": " + post.get("message")
-          + "\n");
-    }
-    tvPosts.setText(sb.toString());
-  }
+					Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+					//					if(alarmSound == null){
+					//						alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+					//						if(alarmSound == null){
+					//							alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+					//						}
+					//					}
+					// build notification
+					// the addAction re-use the same intent to keep the example short
+					Notification.Builder n  = new Notification.Builder(context)
+					.setContentTitle(" Nuevo mensaje!! ")
+					.setContentText(" Tocar para leer. ")
+					.setSmallIcon(R.drawable.ic_launcher)
+					.setContentIntent(pIntent)
+					.setAutoCancel(true);
 
-  // removing the domain name part from email address
-  private String getCreatorName(CloudEntity b) {
-    if (b.getCreatedBy() != null) {
-      return " " + b.getCreatedBy().replaceFirst("@.*", "");
-    } else {
-      return "<anonymous>";
-    }
-  }
+					if(alarmSound != null)
+						n.setSound(alarmSound);
 
-  // post a new message to server
-  public void onSendButtonPressed(View view) {
+					NotificationManager notificationManager = 
+							(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-    // create a CloudEntity with the new post
-    CloudEntity newPost = new CloudEntity("Guestbook");
-    newPost.put("message", etMessage.getText().toString());
+					notificationManager.notify(0, n.getNotification());
 
-    // create a response handler that will receive the result or an error
-    CloudCallbackHandler<CloudEntity> handler = new CloudCallbackHandler<CloudEntity>() {
-      @Override
-      public void onComplete(final CloudEntity result) {
-        posts.add(0, result);
-        updateGuestbookUI();
-        etMessage.setText("");
-        btSend.setEnabled(true);
-      }
+					wl.release();
+				}
 
-      @Override
-      public void onError(final IOException exception) {
-        handleEndpointException(exception);
-      }
-    };
+				enableUI();
+			}
 
-    // execute the insertion with the handler
-    getCloudBackend().insert(newPost, handler);
-    btSend.setEnabled(false);
-  }
+			@Override
+			public void onError(IOException exception) {
+				handleEndpointException(exception);
+			}
+		};
 
-  // handles broadcast message and show a toast
-  @Override
-  public void onBroadcastMessageReceived(List<CloudEntity> l) {
-    for (CloudEntity e : l) {
-      String message = (String) e.get(BROADCAST_PROP_MESSAGE);
-      int duration = Integer.parseInt((String) e.get(BROADCAST_PROP_DURATION));
-      Toast.makeText(this, message, duration).show();
-    }
-  }
+		//getCloudBackend().subscribeToCloudMessage("#dog", handler, 50);
+		//	     CloudQuery cq = new CloudQuery("Guestbook");
+		//	        cq.setLimit(50);
+		//	        cq.setSort(CloudEntity.PROP_CREATED_AT, Order.DESC);
+		//	        cq.setScope(Scope.FUTURE_AND_PAST);
+		//	        getCloudBackend().list(cq, handler);
+
+		// execute the query with the handler
+		getCloudBackend().listByKind("Guestbook", CloudEntity.PROP_CREATED_AT, Order.DESC, 50,
+				Scope.FUTURE_AND_PAST, handler);
+	}
+
+	private void handleEndpointException(IOException e) {
+		Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+		//		btSend.setEnabled(true);
+		enableUI();
+	}
+
+	// convert posts into string and update UI
+	private void updateGuestbookUI() {
+		final StringBuilder sb = new StringBuilder();
+		for (CloudEntity post : posts) {
+			sb.append(sdf.format(post.getCreatedAt()) + getCreatorName(post) + ": " + post.get("message")
+					+ "\n");
+		}
+		tvPosts.setText(sb.toString());
+	}
+
+	// removing the domain name part from email address
+	private String getCreatorName(CloudEntity b) {
+		if (b.getCreatedBy() != null) {
+			return " " + b.getCreatedBy().replaceFirst("@.*", "");
+		} else {
+			return "<anonymous>";
+		}
+	}
+
+	// post a new message to server
+	public void onSendButtonPressed(View view) {
+
+		// create a CloudEntity with the new post
+		CloudEntity newPost = new CloudEntity("Guestbook");
+		newPost.put("message", etMessage.getText().toString());
+
+		// create a response handler that will receive the result or an error
+		CloudCallbackHandler<CloudEntity> handler = new CloudCallbackHandler<CloudEntity>() {
+			@Override
+			public void onComplete(final CloudEntity result) {
+				posts.add(0, result);
+				updateGuestbookUI();
+				etMessage.setText("");
+				enableUI();
+				//				btSend.setEnabled(true);
+			}
+
+			@Override
+			public void onError(final IOException exception) {
+				handleEndpointException(exception);
+			}
+		};
+
+		// execute the insertion with the handler
+		getCloudBackend().insert(newPost, handler);
+		//		btSend.setEnabled(false);
+		disableUI();
+	}
+
+	// handles broadcast message and show a toast
+	@Override
+	public void onBroadcastMessageReceived(List<CloudEntity> l) {
+		for (CloudEntity e : l) {
+			String message = (String) e.get(BROADCAST_PROP_MESSAGE);
+			int duration = Integer.parseInt((String) e.get(BROADCAST_PROP_DURATION));
+			Toast.makeText(this, message, duration).show();
+		}
+	}
+
+	private boolean isApplicationBroughtToBackground() {
+		//		ActivityManager am = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
+		//		List<RunningTaskInfo> tasks = am.getRunningTasks(1);
+		//		if (!tasks.isEmpty()) {
+		//			ComponentName topActivity = tasks.get(0).topActivity;
+		//			if (!topActivity.getPackageName().equals(this.getPackageName())) {
+		//				return true;
+		//			}
+		//		}
+
+		//return false;
+		return true;
+	}
+
+	private void disableUI() {
+		pgbar.animate();
+		pgbar.setVisibility(pgbar.VISIBLE);
+		btSend.setEnabled(false);
+		etMessage.setEnabled(false);
+	}
+
+	private void enableUI() {
+		pgbar.setVisibility(pgbar.INVISIBLE);
+		pgbar.setEnabled(false);
+		btSend.setEnabled(true);
+		etMessage.setEnabled(true);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		this.isAppinForeground = false;
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		this.isAppinForeground = true;
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		test =  true;
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+	}
+
 }
